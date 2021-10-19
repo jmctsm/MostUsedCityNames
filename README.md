9cd0874b9f4aca932a56891d0f5ab0b4ceba2473
jmctsm <james.copeland@gmail.com>
2021-10-19 08:03:15 -0500

    Fixed Cred Parser to error out when wrong.  Added lines to Readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # MostUsedCityNames
 
 Used to query for city names to find the most used city names
+
+If you use this, you have to register for an account with back4app.com and get an app key and a Rest Key
+
+Add your keys to the keys.yml.change file and change the file name to keys.yml
--- cred_parser.py
+++ cred_parser.py
@@ -10,6 +10,15 @@ def parser():
             yaml_dict = yaml.safe_load(key_stream)
             return_dict["rest_api_key"] = yaml_dict[0]["keys"]["rest_api_key"]
             return_dict["application_id"] = yaml_dict[0]["keys"]["application_id"]
+            for return_key in return_dict.keys():
+                if (
+                    return_dict[return_key] == "Put  in the Rest API Key Here"
+                    or return_dict[return_key] == "Put in the Application ID Key Here"
+                ):
+                    print(
+                        f"No key was in the keys.yml file for the {return_key }.  Exiting."
+                    )
+                    exit()
             return return_dict
         except yaml.YAMLError as ex:
             print(ex)
